f7a3252174e1481a5edda532376fc8a851c90c5b
Fedor <Fedor@DESKTOP-TGO8AHQ>
2024-09-13 14:24:20 +0700

    Добавьте файлы проекта.

---
+++ prac1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdio> // из C в С++
+
+//Плохо?
+using namespace std;
+
+#pragma warning( disable : 4996) // Небезопас функ вкл
+
+namespace RED
+{
+
+}
+
+int main()
+{
+    cout << "Hello World!\n";
+    int a, b;
+    
+    //Учитываются пробелы
+    cin >> std::noskipws >> a >> b;
+
+    //(По умолчанию) Игнор все разделитель
+    cin >> a >> b;
+
+    //Безопасный C++
+    //freopen_s
+
+    //Безопасный C++ паралл мьютексы
+    //scanf();
+
+    // По умолчанию ввод на него
+    //scanf_s();
+
+    // Отключить привязку scanf printf
+    //10^6
+    ios_base::sync_with_stdio(false);
+
+
+    
+
+
+}
