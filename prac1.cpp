3f6013c3f2192c6ffa1232a985855f57e6a5dbdd
Fedor <Fedor@DESKTOP-TGO8AHQ>
2024-09-13 15:06:27 +0700

    v 0.2 Создали Игрока  Противника Экран

--- prac1.cpp
+++ prac1.cpp
@@ -1,13 +1,19 @@
 ﻿#include <iostream>
 #include <cstdio>
+#include <random>
 #include <vector>
 
 using namespace std;
 
-struct Srcreen
+//random_device rd;
+//mt19937 gen; // Глобальный генератор случ чисел
+
+struct ScreenController
 {
 	int iSizeW = 10;
 	int iSizeH = 10;
+	
+	string EndMessage = "Game Over, wasted";
 
 	//Сначала Y, потом X
 	vector<vector<int>> arrMap;
@@ -19,8 +25,12 @@ struct Srcreen
 		return char(iData + '0'); // '0' ascii = 48
 	}
 
-	Srcreen()
+	ScreenController(int _iSizeH, int _iSizeW, int _iClearRow)
 	{
+		iSizeH = _iSizeH;
+		iSizeW = _iSizeW;
+		iClearRow = _iClearRow;
+
 		//Иниц. размеров поля
 		//vector начал иниц = вызов констр по умолч (int? 0)
 		arrMap.resize(iSizeH);
@@ -53,17 +63,80 @@ struct Srcreen
 			//cout.flush(); // Лишние затраты // Нужен лишь для передачи управления
 			//cout << endl;
 		}
-
 	}
 
+	void End()
+	{
+		ClearScrean();
+		cout << EndMessage;
+	}
 };
 
-Srcreen fScreen;
+
+struct EnemyController
+{
+	int iPower = 3;
+	ScreenController* ptrScreen;
+
+
+	EnemyController(int _iPower, ScreenController* _ptrScreen)
+	{
+		iPower = _iPower;
+	}
+
+	bool Turn()
+	{
+		int iTurnLimit = iPower;
+		while (true)
+		{
+			if (iTurnLimit <= 0) break;
+			//rand() 0 2^32 - 1
+			int iTargetX = (rand()% ptrScreen->iSizeW);
+			int iTargetY = (rand()% ptrScreen->iSizeH);
+
+
+		}
+
+
+	}
+};
+
+struct PlayerController
+{
+	int iScoore = 0;
+	ScreenController* ptrScreen;
+
+	PlayerController(ScreenController* _ptrScreen)
+	{
+	}
+
+	bool Turn()
+	{
+
+
+	}
+};
+
+ScreenController fScreen(10, 10, 5);
+
 
 int main()
 {
-	fScreen.OutScreen();
+	EnemyController fEnemy(3, &fScreen);
+	PlayerController fPlayer(&fScreen);
 
-	int iPlayerTurn;
-	cin >> iPlayerTurn;
+	//seed random
+	srand(time(0));
+
+	while (true)
+	{
+		if (fEnemy.Turn())
+		{
+			fScreen.End();
+			return 0;
+		}
+		fScreen.OutScreen();
+		fPlayer.Turn();
+		fScreen.OutScreen();
+	}
 }
