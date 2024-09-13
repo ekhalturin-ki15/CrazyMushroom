688deb5298236ccc671606ceba94a91ce7d6d6db
Fedor <Fedor@DESKTOP-TGO8AHQ>
2024-09-13 15:04:44 +0700

    v 0.2 Добавили экран, противника и игрока

--- prac1.cpp
+++ prac1.cpp
@@ -6,41 +6,64 @@ using namespace std;
 
 struct Srcreen
 {
-    int iSizeW = 10;
-    int iSizeH = 10;
+	int iSizeW = 10;
+	int iSizeH = 10;
 
-    //Сначала Y, потом X
-    vector<vector<int>> arrMap;
+	//Сначала Y, потом X
+	vector<vector<int>> arrMap;
 
-    Srcreen()
-    {
+	int iClearRow = 5;
 
-        //Иниц. размеров поля
-        arrMap.resize(iSizeH);
-        for (int Y = 0; Y < iSizeH; ++Y)
-        {
-            arrMap[Y].resize(iSizeW);
-        }
-        //arrMap.assign(iSizeH, vector<int>(iSizeW));
-    }
+	char Out(int iData)
+	{
+		return char(iData + '0'); // '0' ascii = 48
+	}
 
-    void OutScreen()
-    {
+	Srcreen()
+	{
+		//Иниц. размеров поля
+		//vector начал иниц = вызов констр по умолч (int? 0)
+		arrMap.resize(iSizeH);
+		for (int Y = 0; Y < iSizeH; ++Y)
+		{
+			arrMap[Y].resize(iSizeW);
+		}
+		//arrMap.assign(iSizeH, vector<int>(iSizeW));
+	}
 
+	void ClearScrean()
+	{
+		for (int Y = 0; Y < iClearRow; ++Y)
+		{
+			cout << "\n";
+		}
+	}
 
+	void OutScreen()
+	{
+		ClearScrean();
+		for (int Y = 0; Y < iSizeH; ++Y)
+		{
+			for (int X = 0; X < iSizeW; ++X)
+			{
+				cout << Out(arrMap[Y][X]);
+			}
+			cout << "\n";
+			
+			//cout.flush(); // Лишние затраты // Нужен лишь для передачи управления
+			//cout << endl;
+		}
 
-
-
-    }
+	}
 
 };
 
+Srcreen fScreen;
+
 int main()
 {
-    
+	fScreen.OutScreen();
 
-
-
-
-
+	int iPlayerTurn;
+	cin >> iPlayerTurn;
 }
